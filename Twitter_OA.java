c8a2c1c9a268f49d341a42ba54903be341acd12a
mxmingya <mxmingya@gmail.com>
2016-04-20 01:20:09 -0500

    update twtter OA

--- Twitter_OA.java
+++ Twitter_OA.java
@@ -2,4 +2,10 @@
  * Created by maxxx950 on 3/20/16.
  */
 public class Twitter_OA {
+    /*compute a fibonacci sequence and record everything in an array*/
+    /*compute correlation coefficient of three columns of numbers*/
+    /*assume what if each column comtains over 400,000 data, how would you do it?*/
+    public static void main(String[] args){
+
+    }
 }
